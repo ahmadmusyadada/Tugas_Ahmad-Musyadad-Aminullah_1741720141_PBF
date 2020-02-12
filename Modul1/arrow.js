7c250a11e2802878b34befc14b47b9a33fd6e50f
Ahmad Musyadad Aminullah <ahmadmusyadadaminullah@gmail.com>
2020-02-12 07:57:24 +0700

    Membuat Arrow Function 1

---
+++ Modul1/arrow.js
@@ -0,0 +1,8 @@
+// Tanpa arrow
+// function gretting(message){
+//  return alert(`${message} Ganteng!`)
+// }
+
+// Menggunakan arrow
+let gretting = message => alert(`${message} Ganteng!`);
+gretting('Selamat Pagi');
