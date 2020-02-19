ad65b9f12429fbb32ac61391520d70ae55d724ac
Ahmad Musyadad Aminullah <ahmadmusyadadaminullah@gmail.com>
2020-02-19 12:18:56 +0700

    Fixed main page

--- pertemuan-ketiga/ahmadmusyadadaminullah-minggu3/src/App.js
+++ pertemuan-ketiga/ahmadmusyadadaminullah-minggu3/src/App.js
@@ -6,17 +6,31 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <p className="Title">B I O D A T A</p>
         <div class="row">
           <div class="col-5">
           <img src={logo} className="App-logo" alt="logo" />
           </div>
-          <div class="col-2">
+          <div class="col-1">
             <p>Nama:</p>
             <p>TTL:</p>
+            <p>Gender:</p>
+            <p>Alamat:</p>
+            <p>Status:</p>
+            <p>Hobi:</p>
+            <p>Email:</p>
+            <p>Github:</p>
+            
           </div>
-          <div class="col-5">
+          <div class="col-6">
             <p>Ahmad Musyadad Aminullah</p>
             <p>20 August 1999</p>
+            <p>Laki - Laki</p>
+            <p>Jl. Kalpataru No. 49B</p>
+            <p>Mahasiswa</p>
+            <p>Game</p>
+            <p>ahmadmusyadadaminullah@gmail.com</p>
+            <a href="https://github.com/ahmadmusyadada" target="_blank">https://github.com/ahmadmusyadada</a>
           </div>
       </div>
       </header>
