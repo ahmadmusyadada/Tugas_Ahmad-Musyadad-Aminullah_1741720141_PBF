4647550e5f71d3d6522794f42c18ff52164ae832
Ahmad Musyadad Aminullah <ahmadmusyadadaminullah@gmail.com>
2020-02-26 09:15:41 +0700

    Menambahkan file css

--- pertemuan-keempat/src/index.js
+++ pertemuan-keempat/src/index.js
@@ -26,7 +26,7 @@ class Greeting extends React.Component {
     }
 }
 
-ReactDOM.render(<StateFullComponent />, document.getElementById('root'));
+ReactDOM.render(<HelloComponent />, document.getElementById('root'));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
