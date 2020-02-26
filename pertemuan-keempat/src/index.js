2b47181a7e69060b06ddbbfbabc7d9085bb2a60e
Ahmad Musyadad Aminullah <ahmadmusyadadaminullah@gmail.com>
2020-02-26 08:53:11 +0700

    stateless component atau function component

--- pertemuan-keempat/src/index.js
+++ pertemuan-keempat/src/index.js
@@ -4,7 +4,10 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function HelloWorld (){
+    return <p>Ini adalah function component</p>
+}
+ReactDOM.render(<HelloWorld />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
