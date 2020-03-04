72906749dc7dc78e31eeff7efe16c18ebbf8c000
Ahmad Musyadad Aminullah <ahmadmusyadadaminullah@gmail.com>
2020-03-04 09:03:42 +0700

    Mengambil data Post/Artikel dari API

--- pertemuan-kelima/src/container/BlogPost/BlogPost.jsx
+++ pertemuan-kelima/src/container/BlogPost/BlogPost.jsx
@@ -3,11 +3,28 @@ import './BlogPost.css';
 import Post from "./Post";
 
 class BlogPost extends Component{
+    state = {               // Komponen state dari React untuk statefull component
+        listArtikel: []     // Variabel array yang digunakan untuk menyimpan data API
+    }
+
+    componentDidMount(){                                            // Komponen untuk mengecek ketika component telah di mounting, maka panggil API
+        fetch('https://jsonplaceholder.typicode.com/posts')         // Alamat URL API yang ingin kita ambil datanya
+        .then(response => response.json())                          // Ubah response data dari URL API menjadi sebuah data json
+        .then(jsonHasilAmbilDariAPI => {                            // Data json hasil ambil dari API kita masukkan ke dalam listArtikel pada state
+            this.setState({listArtikel: jsonHasilAmbilDariAPI})
+        })
+    }
+
     render(){
         return(
             <div class="post-artikel">
                 <h2>Daftar Artikel</h2>
-                <Post judul="JTI Polinema" isi="Jurusan Teknologi Informasi - Politeknik Negeri Malang"/>
+                {
+                    this.state.listArtikel.map(artikel => {     // Looping dan masukkan untuk setiap data yang ada di listArtikel ke variabel artikel
+                        return <Post judul={artikel.title} isi={artikel.body}/>   // Mappingkan data json dari API sesuai dengan kategorinya
+                    })
+                }
+                
             </div>
         )
     }
