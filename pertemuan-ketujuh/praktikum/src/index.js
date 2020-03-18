6327017c585a95ae8f05ff24d4f200c82138cb9d
Ahmad Musyadad Aminullah <ahmadmusyadadaminullah@gmail.com>
2020-03-18 12:32:53 +0700

    Final Praktikum

--- pertemuan-ketujuh/praktikum/src/index.js
+++ pertemuan-ketujuh/praktikum/src/index.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware, compose } from 'redux'
+import MainReducer from './reducers/MainReducer'
+// import registerServiceWorker from './registerServiceWorker';
+import * as serviceWorker from './serviceWorker';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import Table from './containers/Table'
+import CreateTodo from './containers/CreateTodo'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = compose(window.devToolsExtension ? window.devToolsExtension() : f => f)(createStore)(MainReducer)
+
+ReactDOM.render(<Provider store={store}>
+    <CreateTodo/>
+    <Table/>
+    </Provider>
+    , document.getElementById('root'));
+    serviceWorker.unregister();
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
