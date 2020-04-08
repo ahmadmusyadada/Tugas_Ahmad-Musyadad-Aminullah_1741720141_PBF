0c9dfdad3af15b57217f83155c5a51690bce5e58
Ahmad Musyadad Aminullah <ahmadmusyadadaminullah@gmail.com>
2020-04-08 08:51:44 +0700

    Fix init project

--- pertemuan-kesepuluh/praktikum/src/index.js
+++ pertemuan-kesepuluh/praktikum/src/index.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+import HelloComponent from './component/HelloComponent';
+import StateFullComponent from './container/StateFullComponent';
+import BlogPost from "./container/BlogPost/BlogPost";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+class Greeting extends React.Component {
+    render() {
+        return <h1>Hello, {this.props.name}</h1>;
+    }
+}
 
+ReactDOM.render(<BlogPost />, document.getElementById('content'));
+// ReactDOM.render(<StateFullComponent />, document.getElementById('form'));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
