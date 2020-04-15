ebbc38e98c65f29dc6060e5d2d1636d43cec8c49
Ahmad Musyadad Aminullah <ahmadmusyadadaminullah@gmail.com>
2020-04-15 09:28:30 +0700

    Add feature Join

---
+++ pertemuan-kesebelas/praktikum/src/firebase.config.js
@@ -0,0 +1,10 @@
+export default {
+    apiKey: "AIzaSyAEUlU1W_KoOotW4IB7EpRBODyPMrpLkrs",
+    authDomain: "pbf-modul-10.firebaseapp.com",
+    databaseURL: "https://pbf-modul-10.firebaseio.com",
+    projectId: "pbf-modul-10",
+    storageBucket: "pbf-modul-10.appspot.com",
+    messagingSenderId: "945635927921",
+    appId: "1:945635927921:web:7d26f5ee6c8b9aade738d0",
+    measurementId: "G-F6WEMY4CP6"
+};
