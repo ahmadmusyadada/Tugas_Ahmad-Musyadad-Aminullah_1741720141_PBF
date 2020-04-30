06e4d138e4939e28fb66eb69dc95ab7736b7783e
Ahmad Musyadad Aminullah <ahmadmusyadadaminullah@gmail.com>
2020-04-30 07:11:30 +0700

    Done

---
+++ pertemuan-ketigabelas/praktikum/src/firebase/config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAQvGbxrln1BuJiRqK-Me16sPQ_HwrXKwE",
+    authDomain: "fir-login-dc5ad.firebaseapp.com",
+    databaseURL: "https://fir-login-dc5ad.firebaseio.com",
+    projectId: "fir-login-dc5ad",
+    storageBucket: "fir-login-dc5ad.appspot.com",
+    messagingSenderId: "622236040594",
+    appId: "1:622236040594:web:84207fbf99e8fd204ad54b",
+    measurementId: "G-NY3B81B1GR"
+}
+
+export default firebaseConfig;
